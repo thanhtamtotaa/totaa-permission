3c349a2574b8cab1fab8f2cf9048f5a26a1bed9d
thanhtamtotaa <thanhtamtotaa@gmail.com>
2020-11-26 13:53:42 +0700

    Create routes.php

---
+++ routes/routes.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::middleware(['auth', 'CheckAccount'])->group(function () {
+
+    //Route Admin
+    Route::redirect('admin', '/', 301);
+    Route::group(['prefix' => 'admin'], function () {
+
+        //Route quản lý Role và Quyền
+        Route::redirect('permission', '/', 301);
+        Route::group(['prefix' => 'permission'], function () {
+
+            Route::get('role',  function () {
+                dd(321321);
+            })->name('admin.permission.role');
+
+
+        });
+
+    });
+
+});
+
