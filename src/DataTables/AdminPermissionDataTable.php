843a043e6e211e10803ce83b35aadf52315e4c87
thanhtamtotaa <thanhtamtotaa@gmail.com>
2020-11-26 16:22:44 +0700

    Create AdminPermissionDataTable.php

---
+++ src/DataTables/AdminPermissionDataTable.php
@@ -0,0 +1,150 @@
+<?php
+
+namespace Totaa\TotaaPermission\DataTables;
+
+use Spatie\Permission\Models\Permission;
+use Yajra\DataTables\Html\Button;
+use Yajra\DataTables\Html\Column;
+use Yajra\DataTables\Html\Editor\Editor;
+use Yajra\DataTables\Html\Editor\Fields;
+use Yajra\DataTables\Services\DataTable;
+use Auth;
+
+class AdminPermissionDataTable extends DataTable
+{
+    /**
+     * Build DataTable class.
+     *
+     * @param mixed $query Results from query() method.
+     * @return \Yajra\DataTables\DataTableAbstract
+     */
+    public function dataTable($query)
+    {
+        return datatables()
+            ->eloquent($query)
+            ->addColumn('action', function ($query) {
+                $Action_Icon="<div class='action-div icon-4 px-0 mx-1 d-flex justify-content-around text-center'>";
+
+                if (Auth::user()->user_info->can("edit-permission")) {
+                    $Action_Icon.="<div class='col-3 action-w-50 action-btn' customize-edit-permission='$query->id'><i class='text-indigo fas fa-edit'></i></div>";
+                }
+
+                if (Auth::user()->user_info->can("delete-permission")) {
+                    $Action_Icon.="<div class='col-3 action-w-50 action-btn' customize-delete-permission='$query->id'><i class='text-danger fas fa-trash-alt'></i></div>";
+                }
+
+                $Action_Icon.="</div>";
+
+                return $Action_Icon;
+            })
+            ->editColumn('created_at', function ($query) {
+                return $query->created_at->format("d-m-Y H:i:s");
+            })
+            ->editColumn('updated_at', function ($query) {
+                return $query->updated_at->format("d-m-Y H:i:s");
+            });
+    }
+
+    /**
+     * Get query source of dataTable.
+     *
+     * @param \App\Spatie\Permission\Models\Permission $model
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function query(Permission $model)
+    {
+        $query = $model->newQuery();
+
+        if (!request()->has('order')) {
+            $query->orderBy('id', 'desc');
+        };
+
+        return $query;
+    }
+
+    /**
+     * Optional method if you want to use html builder.
+     *
+     * @return \Yajra\DataTables\Html\Builder
+     */
+    public function html()
+    {
+        return $this->builder()
+                    ->setTableId('permission-table')
+                    ->columns($this->getColumns())
+                    ->dom("<'row'<'col-md-6 col-sm-12'l><'col-md-6 col-sm-12'f>r><'row'<'col-sm-12 table-responsive't>><'row'<'col-md-5 col-sm-12'i><'col-md-7 col-sm-12'p>>")
+                    ->parameters([
+                        "autoWidth" => false,
+                        "lengthMenu" => [
+                            [10, 25, 50, -1],
+                            [10, 25, 50, "Tất cả"]
+                        ],
+                        "order" => [],
+                        'initComplete' => 'function(settings, json) {
+                            var api = this.api();
+
+                            window.addEventListener("dt_draw", function(e) {
+                                api.draw(false);
+                                e.preventDefault();
+                            })
+
+                            api.buttons()
+                                .container()
+                                .appendTo($("#datatable-button"));
+                        }',
+                    ]);
+    }
+
+    /**
+     * Get columns.
+     *
+     * @return array
+     */
+    protected function getColumns()
+    {
+        return [
+            Column::computed('action')
+                    ->exportable(false)
+                    ->printable(false)
+                    ->width(60)
+                    ->addClass('text-center')
+                    ->title("")
+                    ->footer(""),
+            Column::make('name')
+                    ->title("Tên Permission")
+                    ->width(150)
+                    ->searchable(true)
+                    ->orderable(true)
+                    ->footer("Tên Permission"),
+            Column::make('description')
+                    ->title("Mô tả")
+                    ->width(150)
+                    ->searchable(true)
+                    ->orderable(true)
+                    ->footer("Mô tả"),
+            Column::make('group')
+                    ->title("Nhóm")
+                    ->width(150)
+                    ->searchable(true)
+                    ->orderable(true)
+                    ->footer("Nhóm"),
+            Column::computed('created_at')
+                    ->width(200)
+                    ->title("Thời gian tạo")
+                    ->footer("Thời gian tạo"),
+            Column::computed('updated_at')
+                    ->title("Thời gian cập nhật")
+                    ->footer("Thời gian cập nhật"),
+        ];
+    }
+
+    /**
+     * Get filename for export.
+     *
+     * @return string
+     */
+    protected function filename()
+    {
+        return 'Permission_' . date('YmdHis');
+    }
+}
