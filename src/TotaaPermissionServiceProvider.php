1612c964872c640a41156d3d9e24727ab62417d8
thanhtamtotaa <thanhtamtotaa@gmail.com>
2020-11-26 10:48:51 +0700

    Update TotaaPermissionServiceProvider.php

--- src/TotaaPermissionServiceProvider.php
+++ src/TotaaPermissionServiceProvider.php
@@ -16,7 +16,7 @@ class TotaaPermissionServiceProvider extends ServiceProvider
          */
         // $this->loadTranslationsFrom(__DIR__.'/../resources/lang', 'totaa-permission');
         // $this->loadViewsFrom(__DIR__.'/../resources/views', 'totaa-permission');
-        // $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+        $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
         // $this->loadRoutesFrom(__DIR__.'/routes.php');
 
         if ($this->app->runningInConsole()) {
